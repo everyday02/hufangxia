74957819ec4bf7122e8ccffdf9050e57e1bfa782
hehangfighting <hehangdehao@outlook.com>
2017-04-25 23:31:45 +0800

    回忆-03模块

--- js/1/main.js
+++ js/1/main.js
@@ -3,7 +3,8 @@ var X = new Object();
   $window = $(window),
   $body = $('body'),
   $wrapper = $('#recall-wrapper'),
-  $recal01 = $('.recall-01');
+  $recal01 = $('.recall-01'),
+  $recal03_interval= null;
   X = {
     listeners: {
       onLoaded: function() {
@@ -23,12 +24,45 @@ var X = new Object();
       this.listeners.onLoaded();
       this.listeners.bindMoveSectionDown();
     },
+    recall3Fn: function() {
+      if($recal03_interval) clearInterval($recal03_interval);
+      var firstEl = null,
+          preElements = Array.from($('.recall-03 .setp')).reverse();
+
+      firstEl = $(preElements.pop());
+      firstEl.fadeIn('slow');
+
+      $recal03_interval = setInterval(function() {
+        console.info('interval ing...');
+        firstEl.fadeOut().remove();
+        firstEl = $(preElements.pop());
+        firstEl.fadeIn('slow');
+        if(preElements.length == 0) clearInterval($recal03_interval);
+      }, 4000);
+    },
+    afterFullPageScroll: function(anchorLink, index, slideIndex) {
+      console.info(index);
+      switch (index) {
+        case 1:
+          break;
+        case 2:
+          break;
+        case 3:
+          X.recall3Fn();
+          break;
+        case 4:
+          break;
+        default:
+          break;
+      }
+    },
     init: function() {
       console.info('init page script');
 
       $wrapper.fullpage({
         // navigationTooltips: ['提示一','提示二','提示三','提示四'],
-        navigation: true
+        navigation: true,
+        afterLoad: X.afterFullPageScroll
       });
       this.initListenrs();
     }
