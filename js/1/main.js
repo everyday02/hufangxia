80cc4d43211c3a9e8bfbcc4c93567b3ee16eecc2
hehangfighting <hehangdehao@outlook.com>
2017-04-28 15:38:34 +0800

    缩略图

--- js/1/main.js
+++ js/1/main.js
@@ -7,6 +7,8 @@ var X = new Object();
   $recal02 = $('.recall-02'),
   $recal03_interval= null;
   X = {
+    recall2FnFlag: false,
+    recall3FnFlag: false,
     listeners: {
       onLoaded: function() {
         $window.on('load', function() {
@@ -40,16 +42,46 @@ var X = new Object();
       console.info('recall2Fn call...');
       $recal02.find('.map-me-you').css({'opacity': 0}).show().animate({'width': '65%', 'opacity': 1},2000, null, function() {
         $('.boy-wuhan').fadeIn(2500,function() {
-          $('.girl-tianshui').fadeIn('slow');
-          $recal02.find('.map-me-you').animate({'width': '45%'})
+          $('.girl-tianshui').fadeIn('slow',function() {
+            $recal02.find('.map-me-you').animate({'width': '45%'});
+            $('.girl-tianshui').animate({left: '9%'})
+            $('.boy-wuhan').animate({top: '67%', left: '35.5%'})
+          });
+
           $(".map-say-content").show();
           $('.map-say-01').typed({
-            strings: ["两个纬度相差甚远的南北方孩子<br>"],
+            strings: ["两个纬度相差甚远的南北方孩子"],
             typeSpeed: 400,
             backDelay: 500,
             cursorChar: '',
             callback: function() {
+              $('.girl-tianshui').fadeOut();
+              $('.boy-wuhan').fadeOut();
               $('.map-me-you').hide();
+
+              $('.map-beijing-shunyi').css({'opacity': 0}).show().animate({'opacity': 1}, 2000);
+              $('.map-say-02').typed({
+                strings: ["在诺大的北京的顺义区。"],
+                typeSpeed: 400,
+                backDelay: 500,
+                cursorChar: '',
+                callback: function() {
+                  $('.recall-02 .icon-qinglv').fadeIn(3000);
+                  $('.map-say-03').typed({
+                    strings: ["相遇^600 相知^600 相恋。"],
+                    typeSpeed: 400,
+                    backDelay: 500,
+                    cursorChar: '',
+                    callback: function() {
+                      X.recall2FnFlag = true;
+                      $('.icon-zuanshi').fadeIn();
+                      $('.icon-meiguihua').fadeIn('slow');
+                      $('.icon-heart').fadeIn(3000);
+                    }
+                  });
+                }
+              });
+              /*
               $('.map-beijing-shunyi').css({'opacity': 0}).show().animate({'width': '45%', 'opacity': 1}, 2000);
               $('.map-say-02').typed({
                 strings: ["在诺大的北京顺义区相遇<br>"],
@@ -60,6 +92,7 @@ var X = new Object();
 
                 }
               });
+              */
               // '鬼使神差地在繁花似锦的『大北京』相遇'
             }
           });
@@ -70,6 +103,7 @@ var X = new Object();
     recall3Fn: function() {
       if($recal03_interval) clearInterval($recal03_interval);
 
+      $('.more-steps').show();
       var firstEl = null,
           preElements = Array.from($('.recall-03 .setp')).reverse();
 
@@ -83,8 +117,27 @@ var X = new Object();
         firstEl.fadeOut().hide();
         firstEl = $(preElements.pop());
         firstEl.fadeIn('slow');
-        if(preElements.length == 0) clearInterval($recal03_interval);
+        if(preElements.length == 0) {
+          $('.recall-03 .setp').hide();
+          X.recall3FnFlag = true;
+          clearInterval($recal03_interval);
+          X.recall3FnCallBack_photoWall();
+        }
       }, 5000);
+    },
+    recall3FnCallBack_photoWall: function() {
+      console.info('recall3FnCallBack_photoWall');
+      $('.more-and-more').fadeIn(2000, function() {
+        $('.more-and-more-photo-title').animate({top: '-60px'},2800, null,
+        function() {
+          var photopiles = Array.from($('.photopile li')).reverse();
+          photopiles.sort(function() {
+            return (0.5-Math.random());
+          }).forEach(function(item, index) {
+            setTimeout(function() {$(item).animate({'opacity': 1}, 2000)}, 400 * index);
+          });
+        });
+      });
     },
     afterFullPageScroll: function(anchorLink, index, slideIndex) {
       console.info(index);
@@ -92,9 +145,11 @@ var X = new Object();
         case 1:
           break;
         case 2:
+          if(X.recall2FnFlag) return;
           X.recall2Fn();
           break;
         case 3:
+          if(X.recall3FnFlag) return;
           X.recall3Fn();
           break;
         case 4:
